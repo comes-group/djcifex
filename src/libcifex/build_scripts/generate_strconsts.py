b310036115c3f3c9f2a527c621574bdba9146008
lqdev <liquidev@tutanota.com>
2022-01-16 00:12:02 +0100

    bitwise string matching

--- src/libcifex/build_scripts/generate_strconsts.py
+++ src/libcifex/build_scripts/generate_strconsts.py
@@ -1,4 +1,15 @@
-import sys
+import argparse
+
+def truefalse(s):
+   if s == "true": return True
+   else: return False
+
+ap = argparse.ArgumentParser(description="Generates strconsts for the DJ Cifex decoder")
+ap.add_argument("in_file_name")
+ap.add_argument("out_file_name")
+ap.add_argument("--bytewise", type=truefalse)
+ap.add_argument("--endianness", type=str, choices=["little", "big"])
+args = ap.parse_args()
 
 generated = """
 #include <stdbool.h>
@@ -13,17 +24,35 @@ def generate_code_for_strconst(ident, string):
 
    string = bytes(string, "UTF-8")
    conds = []
-   for i in range(0, len(string)):
-      c = string[i]
-      conds.append(f"(input[{i}] == {repr(c)})")
-   cond = ' && '.join(conds)
+   if args.bytewise:
+      n_bytes = 8
+      for i in range(0, len(string), n_bytes):
+         chunk = string[i : (i + n_bytes)]
+         chunk_iter = chunk if args.endianness == "big" else reversed(chunk)
+         hex_bytes = "".join([f"{x:x}" for x in chunk_iter])
+         mask = "0x" + "FF" * len(chunk)
+         if args.endianness == "big":
+            zeroes = "00" * (n_bytes - len(chunk))
+            mask += zeroes
+            hex_bytes += zeroes
+         hex_bytes = "0x" + hex_bytes
+         conds.append(f"((wide_input[{i // n_bytes}] & {mask}) == {hex_bytes})")
+   else:
+      for i in range(0, len(string)):
+         c = string[i]
+         conds.append(f"(input[{i}] == {repr(c)})")
+   cond = " && ".join(conds)
 
-   result += f"static cx_inline bool cx_sc_{ident}_match(const uint8_t *input) {{ return {cond}; }}\n"
+   result += f"""static cx_inline bool cx_sc_{ident}_match(const uint8_t *input) {{
+      const uint64_t *wide_input = (const uint64_t *)input;
+      return {cond};
+   }}
+"""
    result += f"static size_t cx_sc_{ident}_len = {len(string)};\n"
    return result
 
-in_file_name = sys.argv[1]
-out_file_name = sys.argv[2]
+in_file_name = args.in_file_name
+out_file_name = args.out_file_name
 
 with open(in_file_name, "r") as in_file:
    for line in in_file.read().splitlines():
